d02810b2abf18d6489f143403610031d98dbc906
Han Jongchan <whdcks6328@gmail.com>
2018-08-09 01:51:55 +0900

    타이틀에 간단히 텍스트 읽어다 출력하는 구조 설치.

---
+++ Assets/Dialog/DialogManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+
+public class DialogManager : MonoBehaviour {
+    string SampleDialog = @"Assets\Dialog\Sample.txt";
+    //string decodeString;
+    string[] textValue;
+    Text text;
+    int i;
+
+    // Use this for initialization
+    void Start () {
+        i = 0;
+        text = GetComponent<Text>();
+        //string convertTxt = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes("contents"));
+        textValue = System.IO.File.ReadAllLines(SampleDialog);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (i < textValue.Length) { 
+            text.text = (i+1).ToString() + ". " +textValue[i];
+            }
+            else
+            {
+                text.text = "It`s over!";
+            }
+            i++;
+        }
+        
+	}
+}
